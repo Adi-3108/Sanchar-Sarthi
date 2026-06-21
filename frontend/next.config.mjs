2de71f697c1dfb14ad1d5373fb508a0c63c913cf
Adi-3108 <uaditya763@gmail.com>
2026-06-22 03:06:36 +0530

    Resolve Error in Deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: "http://34.172.68.235:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
